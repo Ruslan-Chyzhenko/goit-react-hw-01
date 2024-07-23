cf2df26ba42a81deee14ac367e4d8aa6c6a11968
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-07-23 19:13:36 +0200

    process_revision_005

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,6 +3,7 @@ import FriendListItem from "../FriendListItem/FriendListItem";
 import css from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
+  console.log(friends);
   return (
     <ul className={css.friendsList}>
       {friends.map((friend) => (
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,10 +4,15 @@ import css from "./FriendListItem.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <div>
-      <img src={avatar} alt={`${name}'s avatar`} width="48" />
-      <p>{name}</p>
-      <p className={css.isOnline ? { statusOnline } : { statusOffline }}>
+    <div className={css.friendListItem}>
+      <img
+        src={avatar}
+        alt={`${name}'s avatar`}
+        width="48"
+        className={css.avatar}
+      />
+      <p className={css.name}>{name}</p>
+      <p className={isOnline ? css.statusOnline : css.statusOffline}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
